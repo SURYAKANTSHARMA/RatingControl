8bef6ffda2998ac76b97cd97055e128268af336b
IOS Developer <iosdeveloper@ioss-mac-mini-2.local>
2018-05-18 13:15:19 +0530

    Initial Commit

---
+++ RatingControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RatingControl
+//
+//  Created by IOS Developer on 18/05/18.
+//  Copyright © 2018 iOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
